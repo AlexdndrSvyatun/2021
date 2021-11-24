396d467b5feacd4a15ff2b46035f1bed79b8b8e9
AlexandrSvyatun <alecksandrtd@gmail.com>
2021-11-24 09:37:13 +0200

    .

---
+++ lb11.py
@@ -0,0 +1,42 @@
+from math import *
+from numpy import *
+import sympy as sp
+from matplotlib import style
+
+x = sp.symbols('x')
+
+f = sp.sin(sp.cos(x))
+n = 3
+
+def taylor(x):
+
+    d1 = sp.diff(f, x)
+    d = d1
+    y = f
+
+    i = 1
+    while i <= n:
+        y = y + d*((x-0)**i)/factorial(i)
+        d = sp.diff(d, x)
+        i += 1
+    
+    print("\ny = ", y , "\n")
+    return y
+
+style.use('seaborn-whitegrid')
+
+taylor_x = taylor(x)
+
+p1 = sp.plot(f, (x, -2, 2),
+title = "Polynom",
+ylabel = "y", label = "f(x)", line_color = "#00e1ff",
+show = False, legend = True)
+
+p2 = sp.plot(taylor_x, (x, -2, 2), label = "ff(x)",
+line_color = "#000000", show = False)
+
+p3 = sp.plot(x, (x, -2, 2), show = False, line_color = "#ff7b00")
+
+p1.append(p2[0])
+p1.append(p3[0])
+p1.show()
