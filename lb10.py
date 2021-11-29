5f7e85c9f28df42c8d96a36968c7f2f2bac1eadb
AlexandrSvyatun <alecksandrtd@gmail.com>
2021-11-29 13:56:56 +0200

    /

---
+++ lb10.py
@@ -0,0 +1,116 @@
+import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+from numpy import *
+import sympy as sp
+from matplotlib import style
+
+x = [0, 0.2, 0.5, 0.9, 1.5]
+y = [1.75, 2.68, 1.24, 0.72, 1.35]
+
+
+
+xs = linspace(x[0], x[-1], 100)
+
+x_i = sp.symbols('x_i')
+
+h = []
+m = []
+k = []
+alpha = [0, 0]
+beta = [0, 0]
+a = y
+b = [0]
+c = [0]
+d = [0]
+
+ys = []
+
+def spline_fun(xk):
+
+    for i in range(len(x)):
+        h_i = x[i] - x[i-1]
+        h.append(h_i)
+   
+
+    for i in range(len(h)):
+        m_i = 2 * (h[i-1] + h[i])
+        m.append(m_i)
+    
+
+    for i in range(len(h)):
+        k_i = 3 * ( ((y[i] - y[i-1]) / h[i]) - ((y[i-1] - y[i-2]) / h[i-1]) )
+        k.append(k_i)
+
+    for i in range (2, len(h)):
+        alpha_i = (k[i] - h[i-1]*alpha[i-1]) / (m[i] - h[i-1]*beta[i-1])
+        alpha.append(alpha_i)
+        beta_i = h[i] / (m[i] - h[i-1]*beta[i-1])
+        beta.append(beta_i)
+
+    for i in range (1, len(alpha)):
+        c_i = alpha[-i] - beta[-i]*c[-i]
+        c.insert(0, c_i)
+
+    i = len(c) - 1
+    while i >= 0:
+        d_i = (c[i]-c[i-1]) / (3 * h[i])
+        d.insert(0, d_i)
+        i-=1
+
+    i = len(c) - 1
+    while i >= 0:
+        b_i = ((y[i] - y[i-1]) / h[i]) - (((c[i] + 2*c[i-1]) * h[i] )/ 3)
+        b.insert(0, b_i)
+        i -= 1
+    #print(b)
+
+    print('\nThe spline:\n')
+    for i in range(len(c)-1):
+        s_i = a[i] + b[i+1]*(x_i - x[i]) + c[i]*((x_i-x[i])**2) + d[i+1]*((x_i-x[i])**3)
+        print(s_i)
+
+    def spline(i, x_j):
+        s_ij = a[i] + b[i+1]*(x_j - x[i]) + c[i]*((x_j-x[i])**2) + d[i+1]*((x_j-x[i])**3)
+        return s_ij
+
+    print('\n\nValues of the spline at given points:\n')
+    for i in range(len(x)-1):
+        print(spline(i, x[i]))
+    print('\n')
+
+    i = len(xk)-1 
+    j = len(x)-1
+
+    while i >= 0:
+
+        while (xk[i] >= x[j]):
+            yi = spline(j, xk[i])
+            ys.insert(0, yi)
+            if i == 0:
+                break
+            i -= 1
+
+        if j == 0:
+           break
+        j -= 1 
+
+    return ys
+
+xsl = xs.tolist()
+spl = spline_fun(xsl)
+
+spl1 = UnivariateSpline(x, y)
+
+style.use('seaborn-whitegrid')
+
+plt.plot( xs, spl, color = '#316879')
+plt.plot(xs, spl1(xs), '--', color = '#7fe7dc')
+plt.plot(x, y, '*', color = '#f47a60' )
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Spline plot')
+plt.legend(['my spline', 'UnivariateSpline', 'input data'])
+
+plt.show()
+
